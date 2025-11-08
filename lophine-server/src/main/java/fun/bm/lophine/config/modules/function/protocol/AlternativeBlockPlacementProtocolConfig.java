7bbae3d0db35bfb2b49b0e371769969ec47a75a8
Bacteriawa <A3167717663@hotmail.com>
2025-11-08 18:04:43 +0800

    Add Alternative block placement Protocol

---
+++ lophine-server/src/main/java/fun/bm/lophine/config/modules/function/protocol/AlternativeBlockPlacementProtocolConfig.java
@@ -0,0 +1,22 @@
+package fun.bm.lophine.config.modules.function.protocol;
+
+import me.earthme.luminol.config.IConfigModule;
+import me.earthme.luminol.config.flags.ConfigClassInfo;
+import me.earthme.luminol.config.flags.ConfigInfo;
+import me.earthme.luminol.enums.EnumConfigCategory;
+
+@ConfigClassInfo(category = EnumConfigCategory.FUNCTION, name = "alternative_block_placement", directory = {"protocol"})
+public class AlternativeBlockPlacementProtocolConfig implements IConfigModule {
+    @ConfigInfo(name = "enabled", comments = """
+            Specify the precise placement protocol type
+            NONE Disable precise placement protocol
+            CARPET Precise placement protocol version 2
+            CARPET_FIX Enhanced precise placement protocol version 2 (requires MasaGadget installed on client)
+            LITEMATICA Precise placement protocol version 3""")
+    public static AlternativePlaceType alternativeBlockPlacement = AlternativePlaceType.NONE;
+
+    public enum AlternativePlaceType {
+        NONE, CARPET, CARPET_FIX, LITEMATICA
+    }
+}
+
