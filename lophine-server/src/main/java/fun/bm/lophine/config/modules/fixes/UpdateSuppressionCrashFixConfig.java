2d0c60eb4bc686c103e198339b9cb6054ab72127
Bacteriawa <A3167717663@hotmail.com>
2025-11-15 01:57:42 +0800

    add cce update suppression & prevent crashes caused by update suppression

---
+++ lophine-server/src/main/java/fun/bm/lophine/config/modules/fixes/UpdateSuppressionCrashFixConfig.java
@@ -0,0 +1,15 @@
+package fun.bm.lophine.config.modules.fixes;
+
+import me.earthme.luminol.config.IConfigModule;
+import me.earthme.luminol.config.flags.ConfigClassInfo;
+import me.earthme.luminol.config.flags.ConfigInfo;
+import me.earthme.luminol.config.flags.TransformedConfig;
+import me.earthme.luminol.enums.EnumConfigCategory;
+
+@ConfigClassInfo(category = EnumConfigCategory.FIXES, name = "update-suppression-crash-fix")
+public class UpdateSuppressionCrashFixConfig implements IConfigModule {
+    @TransformedConfig(name = "enabled", directory = {"serevr", "update-suppression-crash-fix"})
+    @ConfigInfo(name = "enabled", comments = """
+            Should crashes caused by update suppression be prevented?""")
+    public static boolean enabled = true;
+}
