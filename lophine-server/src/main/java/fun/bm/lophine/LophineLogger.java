c07dde1c0b76132125ed7ddf20f11370e566cd2b
xiaoxijun <A3167717663@hotmail.com>
2025-11-19 00:04:34 +0800

    feat: CCE Update Suppression support and other dependences (#84)
    add cce update suppression & prevent crashes caused by update suppres & dustTrapdoorReintroduced & instantBlockUpdater & old Block remove behaviour & do not reset placed block on exception & do not prevent block entity and entity crash at LevelChunk (#84)
    
    * add cce update suppression & prevent crashes caused by update suppression
    
    * no need for this transformed flag
    
    * Add Redstone ignore upwards update
    
    * fix up license
    
    * move config
    
    * instant Block Updater
    
    * feat: Update Suppression!
    
    * Leaves: Do not reset placed block on exception & Do not prevent block entity and entity crash at LevelChunk
    
    ---------
    
    Co-authored-by: Helvetica Volubi <suisuroru@blue-millennium.fun>
    Co-authored-by: Helvetica Volubi <88063803+Suisuroru@users.noreply.github.com>

---
+++ lophine-server/src/main/java/fun/bm/lophine/LophineLogger.java
@@ -0,0 +1,10 @@
+package fun.bm.lophine;
+
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
+
+import java.util.logging.Level;
+
+public class LophineLogger {
+    public static final Logger LOGGER = LogUtils.getClassLogger(); // only provided for some Logger can not be initialized
+}
